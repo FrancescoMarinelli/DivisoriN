b8a8a3ffc5b7a348216aabae1a90b7272a0b686d
francesco marinelli <francescomarinelli04@gmail.com>
2020-11-30 15:29:27 +0100

    Aggiungere i file di progetto.

---
+++ ProvaCalcolo/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Gestione;
+
+namespace ProvaCalcolo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = Divisore.LeggiNum();
+            Divisore.StampaDivisori(n);
+
+        }
+    }
+}
